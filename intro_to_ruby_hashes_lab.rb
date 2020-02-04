01e69577f366f6cf23187d40a2d9e7feaf42c737
Marvin Reyes <reyemarv@gmail.com>
2020-02-04 00:56:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,11 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-  	monopoly = {:railroads => {:pieces => 4},
-  	{:names => {}}
+  	monopoly = {:railroads => {
+    :pieces => "4",
+  	:names => {}
+  	  
+  	}
   	}
   	  
 	monopoly 
